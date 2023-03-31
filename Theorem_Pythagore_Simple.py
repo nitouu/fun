c9dbe2fc04d5860f2604ca6f74026f926de488a4
nitouu <119036894+nitouu@users.noreply.github.com>
2023-03-31 18:19:56 +0200

    Update Theorem_Pythagore_Simple.py

--- Theorem_Pythagore_Simple.py
+++ Theorem_Pythagore_Simple.py
@@ -1,15 +1,18 @@
-import math
 import turtle
 
 
 fenetre = turtle.Turtle()
 
-# Theorem of Pythagore
+# Theorem of Pythagore - To understand under python language.
 
 #
 #[TO REMEMBER] In a right triangle, the square of the hypotenuse is equal to the sum of the squares of the other 2 sides.
 #
 
+#Soon:
+#-Float and Int
+#-Trigonometry's Function (Cos,Tan,Sin) [For the calculations of the Angle's (Euler's Angle, Quaternion ?)]
+#...
 
 
 # [B](Adjacent)' (Cm)
@@ -22,8 +25,8 @@ fenetre = turtle.Turtle()
 #   (Opposite)[A](Cm)
 
 
-AB=float(input())
-AC=float(input())
+AB=int(input())
+AC=int(input())
 
 #AB = Adjacent
 #AC = Opposite
@@ -32,29 +35,51 @@ AC=float(input())
 #Beta = CAB's angle
 #Alpha = BAC's angle
 
+def multiplication(a,b):
+    s=0
+    if a==0 or b==0:
+        return 0
+    if a<0:
+        return -multiplication(-a,b)
+    if b<0:
+        return -multiplication(a,-b)
+    for i in range(a):
+        s = s+b
+    return s
+
+def square(n):
+    s=1
+    s=s*multiplication(n,n)
+    return s
+
+def square_root(x):
+    x=x**(1/2)
+    return x
+
 def Pythagore(AB,AC,CAB,BAC):
 
-    BC=0 #Distance AB+AC
+    BC=0.0 #Distance AB+AC
 
-    BC = AB**2+AC**2+BC
-    BC = math.sqrt(BC)
+    BC = square(AB)+square(AC)+BC
+
+    BC = square_root(BC)
 
     print("BC : ",BC,"cm.")
 
-    print("Angle(CAB):", CAB, "Angle(BAC):", BAC)
+    print("Angle(CAB):", CAB,"°", "Angle(BAC):", BAC,"°")
 
     #Will draw the right triangle.
 
-    fenetre.forward(AC)
+    #fenetre.forward(AC)
 
-    fenetre.left(CAB)
-    fenetre.forward(BC)
+    #fenetre.left(CAB)
+    #fenetre.forward(BC)
 
-    fenetre.left(BAC)
-    fenetre.forward(AB)
+    #fenetre.left(BAC)
+    #fenetre.forward(AB)
 
-    turtle.done()
+    #turtle.done()
 
 
 
-print(Pythagore(AB,AC))
+print(Pythagore(AB,AC,90,90))
